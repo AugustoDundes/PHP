73f1d0d9d76dc32fd03d9e45cfb1504494bb0381
AugustoDundes <gutodundes@hotmail.com>
2024-03-23 11:08:39 -0300

    Update index3.php

--- index3.php
+++ index3.php
@@ -6,31 +6,7 @@
     <title>Document</title>
 </head>
 
-    <form action="ex2.php" method="post">
-        <p>
-        <label>Informe o valor 1:</label>
-        <input type="text" name="valor1">
-        </p>
-        <label>Informe o valor 2:</label>
-        <input type="text" name="valor2">
-        <p>
-        <label>Informe o valor 3:</label>
-        <input type="text" name="valor3">
-        </p>
-        <label>Informe o valor 4:</label>
-        <input type="text" name="valor4">
-        <p>
-        <label>Informe o valor 5:</label>
-        <input type="text" name="valor5">
-        </p>
-        <label>Informe o valor 6:</label>
-        <input type="text" name="valor6">
-        <p>
-        <label>Informe o valor 7:</label>
-        <input type="text" name="valor7">
-        </p>
-        <button type="submit">Enviar</button>
-    </form>
+
 
 </body>
 </html>
